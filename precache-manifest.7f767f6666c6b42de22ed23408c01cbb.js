a5d5a98bfcd766cb477e15a7566080a7039dae64
Henrique Dário <henrique.dariov@gmail.com>
2019-08-11 04:55:50 -0300

    Updates

---
+++ precache-manifest.7f767f6666c6b42de22ed23408c01cbb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f40aec3978b27f466169ec2ce54a92a2",
+    "url": "/Portifolio/index.html"
+  },
+  {
+    "revision": "5f10ce061e28ad5fdb45",
+    "url": "/Portifolio/static/css/main.4bd53da2.chunk.css"
+  },
+  {
+    "revision": "ea448226a951547b69f4",
+    "url": "/Portifolio/static/js/2.8d63160b.chunk.js"
+  },
+  {
+    "revision": "5f10ce061e28ad5fdb45",
+    "url": "/Portifolio/static/js/main.ecb0dd05.chunk.js"
+  },
+  {
+    "revision": "5f654b47d5365fabacd3",
+    "url": "/Portifolio/static/js/runtime~main.4eadf463.js"
+  },
+  {
+    "revision": "c6060c41c0d9877f9c7aac7578c9c732",
+    "url": "/Portifolio/static/media/DarioProd.c6060c41.png"
+  }
+]);
